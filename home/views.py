d2a37e63ba9f3fd7d526bdacd6821ca504bee754
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-07 00:53:58 +0000

    Created Painting model, view and home templates

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
+from .models import Painting
 
-# Create your views here.
-
-def index(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+def painting_list(request):
+    paintings = Painting.objects.all()
+    return render(request, 'home/index.html', {'paintings': paintings})
