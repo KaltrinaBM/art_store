6677f2dc834adede4cf9b56f2a903c5ca20ef169
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-08 01:04:37 +0000

    Add pagination, CSS style for media and static

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Painting
 
 def painting_list(request):
     paintings = Painting.objects.all()
-    return render(request, 'home/index.html', {'paintings': paintings})
+    paginator = Paginator(paintings, 6)  # Show 6 paintings per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home/index.html', {'page_obj': page_obj})
