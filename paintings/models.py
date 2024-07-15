63bcdf80f9d7bb97d2f8d8fca31bb95f06dccad5
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-15 01:55:58 +0000

    Add Sku field

--- paintings/models.py
+++ paintings/models.py
@@ -5,6 +5,7 @@ class Painting(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='paintings/', null=True, blank=True)
+    #sku = models.CharField(max_length=100, blank=True, default='') 
 
     def __str__(self):
         return self.title
