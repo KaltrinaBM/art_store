2b79555306e287c5d1ec185712acbb4386aabd58
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-16 00:30:43 +0000

    Add Sku and customize admin

--- paintings/admin.py
+++ paintings/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Painting
 
-@admin.register(Painting)
 class PaintingAdmin(admin.ModelAdmin):
     list_display = ('title', 'price')
+
+    ordering = ('sku',)
+
     search_fields = ('title', 'description')
+
+admin.site.register(Painting, PaintingAdmin)
--- paintings/models.py
+++ paintings/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import uuid
 
 class Painting(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='paintings/', null=True, blank=True)
-    #sku = models.CharField(max_length=100, blank=True, default='') 
+    image = models.ImageField(upload_to='paintings/', default='paintings/placeholder.jpg')
+    sku = models.CharField(max_length=8, unique=True, default=uuid.uuid4().hex[:8])
 
     def __str__(self):
         return self.title
