f6b956c2a893f9bb0ae74eda792bb82df037fb11
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-19 22:46:05 +0000

    Fixed errors with templates and urls for painting app

--- paintings/urls.py
+++ paintings/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.painting_list, name='painting_list'),
     path('all/', views.all_paintings, name='all_paintings'),
     path('<int:pk>/', views.painting_detail, name='painting_detail'),
 ]
--- paintings/views.py
+++ paintings/views.py
@@ -1,29 +1,36 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 from .models import Painting
 
 def painting_list(request):
     paintings = Painting.objects.all()
-    paginator = Paginator(paintings, 6)  # Show 6 paintings per page.
+    paginator = Paginator(paintings, 6)  
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'home/index.html', {'page_obj': page_obj})
 
-def painting_detail(request, pk):
-    painting = get_object_or_404(Painting, pk=pk)
-    return render(request, 'paintings/painting_detail.html', {'painting': painting})
+# def all_paintings(request):
+#     sort_by = request.GET.get('sort_by', 'title')
+#     order = request.GET.get('order', 'asc')
+#     order_prefix = '' if order == 'asc' else '-'
+#     paintings = Painting.objects.all().order_by(f'{order_prefix}{sort_by}')
+#     paginator = Paginator(paintings, 12)  
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {
+#         'page_obj': page_obj,
+#         'sort_by': sort_by,
+#         'order': order
+#     }
+#     return render(request, 'home/all_paintings.html', context)
 
 def all_paintings(request):
-    sort_by = request.GET.get('sort_by', 'title')
-    order = request.GET.get('order', 'asc')
-    order_prefix = '' if order == 'asc' else '-'
-    paintings = Painting.objects.all().order_by(f'{order_prefix}{sort_by}')
+    paintings = Painting.objects.all()
     paginator = Paginator(paintings, 12)  # Show 12 paintings per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj': page_obj,
-        'sort_by': sort_by,
-        'order': order
-    }
-    return render(request, 'home/all_paintings.html', context)
+    return render(request, 'paintings/all_paintings.html', {'page_obj': page_obj})
+
+def painting_detail(request, pk):
+    painting = get_object_or_404(Painting, pk=pk)
+    return render(request, 'paintings/painting_detail.html', {'painting': painting})
