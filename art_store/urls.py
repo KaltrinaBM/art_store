75ecc5bf09a9c216e13162f80403144fdbdff916
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-07-24 01:00:49 +0000

    Added Bag app, views, urls, templates

--- art_store/urls.py
+++ art_store/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),  # Django Allauth URLs for user authentication
     path('paintings/', include('paintings.urls')), 
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
---
+++ bag/templatetags/__init__.py
@@ -0,0 +1,1 @@
+from .bag_tags import *
