d1572f45ffc0e6f5fadcbba471d17d8927c53c27
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-08-07 21:41:07 +0000

    Remove delivery costs

--- paintings/static/paintings/js/back_to_top.js
+++ paintings/static/paintings/js/back_to_top.js
@@ -1,17 +1,17 @@
-window.onload = function() {
-    const backToTopButton = document.getElementById('back-to-top');
+// window.onload = function() {
+//     const backToTopButton = document.getElementById('back-to-top');
 
-    if (backToTopButton) {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 300) {
-                backToTopButton.style.display = 'block';
-            } else {
-                backToTopButton.style.display = 'none';
-            }
-        });
+//     if (backToTopButton) {
+//         window.addEventListener('scroll', () => {
+//             if (window.scrollY > 300) {
+//                 backToTopButton?.style.display = 'block';
+//             } else {
+//                 backToTopButton?.style.display = 'none';
+//             }
+//         });
 
-        backToTopButton.addEventListener('click', () => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        });
-    }
-};
+//         backToTopButton?.addEventListener('click', () => {
+//             window.scrollTo({ top: 0, behavior: 'smooth' });
+//         });
+//     }
+// };
