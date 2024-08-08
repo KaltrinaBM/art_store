1588a2b50bcaa6c310d9ac80fca2d8f10a7fbf7e
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-08-08 21:15:39 +0000

    Secure user views

--- paintings/views.py
+++ paintings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Painting
 from django.core.paginator import Paginator
 from .forms import PaintingForm
+from django.contrib.auth.decorators import login_required
 
 def all_paintings(request):
     sort_by = request.GET.get('sort_by', 'title')  
@@ -24,19 +25,17 @@ def all_paintings(request):
     return render(request, 'paintings/all_paintings.html', context)
 
 
-# def all_paintings(request):
-#     paintings = Painting.objects.all()
-#     paginator = Paginator(paintings, 12)  # Show 12 paintings per page.
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     return render(request, 'paintings/all_paintings.html', {'page_obj': page_obj})
 
 def painting_detail(request, pk):
     painting = get_object_or_404(Painting, pk=pk)
     return render(request, 'paintings/painting_detail.html', {'painting': painting})
 
+@login_required
 def add_painting(request):
     """ Add a painting to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     form = PaintingForm()
     if request.method == 'POST':
         form = PaintingForm(request.POST, request.FILES)
@@ -57,9 +56,12 @@ def add_painting(request):
     return render(request, template, context)
 
 
-
+@login_required
 def edit_painting(request, painting_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     painting = get_object_or_404(Painting, pk=painting_id)
     if request.method == 'POST':
         form = PaintingtForm(request.POST, request.FILES, instance=painting)
@@ -82,9 +84,12 @@ def edit_painting(request, painting_id):
     return render(request, template, context)
 
 
-
+@login_required
 def delete_painting(request, painting_id):
     """ Delete a painting from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     painting = get_object_or_404(Painting, pk=painting_id)
     painting.delete()
     messages.success(request, 'Painting deleted!')
